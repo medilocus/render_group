7d8660634646dcd7a415ac041db8273b1370a249
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-02-19 14:01:38 -0800

    Props.

---
+++ server/props.py
@@ -0,0 +1,41 @@
+#
+#  Render Group
+#  Blender add-on to create a local render farm.
+#  Copyright Medilocus 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import bpy
+from bpy.types import PropertyGroup
+from bpy.props import BoolProperty, IntProperty, FloatProperty, StringProperty, EnumProperty, PointerProperty
+
+
+class RenderGroupServer(PropertyGroup):
+    pass
+
+
+classes = (
+    RenderGroupServer,
+)
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    bpy.types.Scene.render_group_server = PointerProperty(type=RenderGroupServer)
+
+def unregister():
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+    del bpy.types.Scene.render_group_server
